f8966b0fec769798239adbfcda693f0ab4039ba8
Mert <59804879+MertKacar99@users.noreply.github.com>
2025-03-16 20:31:42 +0300

    assa

---
+++ src/main/java/com/merkacar/model/Employee.java
@@ -0,0 +1,18 @@
+package com.merkacar.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
