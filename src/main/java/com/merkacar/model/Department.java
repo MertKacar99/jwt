ceae4a391115717cf541519d22f04fe983eb4328
Mert <59804879+MertKacar99@users.noreply.github.com>
2025-03-19 17:47:06 +0300

    Department,Employee,User

--- src/main/java/com/merkacar/model/Department.java
+++ src/main/java/com/merkacar/model/Department.java
@@ -6,16 +6,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "employee")
+@Table(name = "department")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-public class Employee {
+@NoArgsConstructor
+public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String firstName;
-    private String lastName;
-    @ManyToOne
-    private Department department;
+    private  Long id;
+    private  String name;
+    private  String location;
 }
--- src/main/java/com/merkacar/starter/JwtApplicationStarter.java
+++ src/main/java/com/merkacar/starter/JwtApplicationStarter.java
@@ -2,7 +2,13 @@ package com.merkacar.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+//@ComponentScan(basePackages = {"com.mertkacar"})
+@EntityScan(basePackages = {"com.merkacar"})
+//@EnableJpaRepositories(basePackages = {"com.mertkacar"})
 @SpringBootApplication
 public class JwtApplicationStarter {
 
